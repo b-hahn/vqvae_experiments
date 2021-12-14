3e7b9d63daaaced05b23b23000a7c2fe45b319b2
Benjamin Hahn <benjamin.hahn1@gmail.com>
2021-12-14 09:54:54 +0000

    Add generate_samples code.
    This doesn't work without sampling from a powerful prior such as PixelCNN.

--- generate_samples.py
+++ generate_samples.py
@@ -1,3 +1,5 @@
+import pickle
+
 import haiku as hk
 import jax
 import jax.numpy as jnp
@@ -6,22 +8,38 @@ from config import Config
 from vqvae import VQVAEModel, Encoder, Decoder
 import visualization
 
+def load(path) -> hk.Params:
+  with open(path, 'rb') as fp:
+    return pickle.load(fp)
+
 def generate_samples():
     seed = 63
     key = jax.random.PRNGKey(seed)
 
     cfg = Config()
 
+    # load params
+    params = load('/home/ben/vqvae_experiments/run_2021-11-24-10-57-00/weights.pkl_8000.pkl')
+
+    # 4. Put parameters back onto the device.
+    params = jax.device_put(params)
+
+    # 5. Use them as you like.
+    # f.apply(params, None, x)
+
     forward_fn = hk.transform_with_state(forward)
     key, subkey = jax.random.split(key)
-    params, state = forward_fn.init(key, jax.random.normal(subkey, shape=(1,32,32,3)), is_training=False, cfg=cfg)
-    train_reconstructions = forward_fn.apply(params, state, key, None, is_training=False)
+    _, state = forward_fn.init(key, jax.random.normal(subkey, shape=(1,32,32,3)), is_training=False, cfg=cfg)
+    # params, state = forward_fn.init(key, jax.random.normal(subkey, shape=(1,32,32,3)), is_training=False, cfg=cfg)
+    train_reconstructions = forward_fn.apply(params, state, key, None, is_training=False, cfg=cfg)[0]
+    visualization.visualize_samples(train_reconstructions)
+    print('done')
 
 
-@jax.jit
-def sample(self, params, state, data):
-    model_output, state = self.forward.apply(params, state, None, data, is_training=False)
-    return state, model_output
+# @jax.jit
+# def sample(self, params, state, data):
+#     model_output, state = self.forward.apply(params, state, None, data, is_training=False)
+#     return state, model_output
 
 def forward(data, is_training, cfg):
     encoder = Encoder(cfg.num_hiddens, cfg.num_residual_layers, cfg.num_residual_hiddens)
@@ -44,7 +62,7 @@ def forward(data, is_training, cfg):
                     data_variance=train_data_variance)
 
     # return model(data['image'], is_training)
-    return model.sample(data)
+    return model.sample(key=hk.next_rng_key())
 
 if __name__ == '__main__':
     generate_samples()
